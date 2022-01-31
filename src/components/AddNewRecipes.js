3e9a6d6aa9b1f66d20f49044a83e159e80e1bb3d
David Conley <dconley1989@gmail.com>
2022-01-31 15:31:40 -0700

    added functionality to the AddNewRecipes page

--- src/components/AddNewRecipes.js
+++ src/components/AddNewRecipes.js
@@ -1,6 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 const AddNewRecipes = () => {
+  const [recipe, setRecipe] = useState({
+    title: "",
+    source: "",
+    ingredients: "",
+    instructions: "",
+    category: "",
+  });
+
+  const handleChange = (e) => {
+    setRecipe({
+      ...recipe,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div>
       <h1>Add New Recipe</h1>
@@ -8,25 +28,50 @@ const AddNewRecipes = () => {
         <form>
           <label>
             Title
-            <input type="text" />
+            <input
+              type="text"
+              name="title"
+              value={recipe.title}
+              onChange={handleChange}
+            />
           </label>
           <label>
             Source
-            <input type="text" />
+            <input
+              type="text"
+              name="source"
+              value={recipe.source}
+              onChange={handleChange}
+            />
           </label>
           <label>
             Ingredients
-            <input type="text" />
+            <input
+              type="text"
+              name="ingredients"
+              value={recipe.ingredients}
+              onChange={handleChange}
+            />
           </label>
           <label>
             Instructions
-            <input type="text" />
+            <input
+              type="text"
+              name="instructions"
+              value={recipe.instructions}
+              onChange={handleChange}
+            />
           </label>
           <label>
             Category
-            <input type="text" />
+            <input
+              type="text"
+              name="category"
+              value={recipe.category}
+              onChange={handleChange}
+            />
           </label>
-          <button>Submit</button>
+          <button onSubmit={handleSubmit}>Submit</button>
         </form>
       </div>
     </div>
