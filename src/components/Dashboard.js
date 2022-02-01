cce1888cae66b17747fd835433c81bd0ba16436e
David Conley <dconley1989@gmail.com>
2022-01-31 21:34:54 -0700

    added state and helper functions for dashboard component

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,12 +1,32 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const Dashboard = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://bloomtechrecipebook.herokuapp.com/api/recipes")
+      .then((resp) => console.log(resp))
+      .catch((err) => console.log(err));
+  }, []);
+
+  const handleChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    recipes.filter((recipe) => recipe === searchTerm);
+  };
+
   return (
     <div>
       <h1>Your Recipes!</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
-          <input type="search" />
+          <input type="search" value={searchTerm} onChange={handleChange} />
           <button>Search</button>
         </div>
       </form>
