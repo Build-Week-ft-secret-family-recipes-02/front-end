a8f379ee6cab71ebbdbb88bd1f26235f43867148
David Conley <dconley1989@gmail.com>
2022-02-03 14:21:45 -0700

    added helper functions and axios call for login

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -2,50 +2,78 @@ import React, { useState } from "react";
 import "../styles/LoginPage.css";
 import axios from "axios";
 
-
 const LoginPage = () => {
-    const [error, setError] = useState(false);
-    const [form, setForm] = useState({username: "",password: ""});
-    
-    return (
-        <div className="login-page">
-            <div className="login-box">
-                <div className="login-top">
-                    <div className="icon"></div>
-                    <br />
-                    <span className="welcome-message">Welcome Chef!</span>
-                </div>
-                <div className="login-bottom">
-                    <form className="login-form">
-                        <div className="input-group">
-                            <input 
-                            type="text"
-                            name="username"
-                            autoComplete="off"
-                            className="input-field"
-                            placeholder="Username"
-                            />
-                            <label className="input-label"><span>Username</span></label>
-                        </div>
-                        <div className="input-group">
-                            <input 
-                            type="password"
-                            name="password"
-                            className="input-field"
-                            placeholder="Password"
-                            />
-                            <label className="input-label"><span>Password</span></label>
-                            {error && <span>*Your username or password is incorrect</span>}
-                            <br />
-                            <br />
-                            <span>Not a member? <a href="/">Sign Up</a></span>
-                        </div>
-                            <button>Log In</button>
-                    </form>
-                </div>
-            </div>
+  const [error, setError] = useState(false);
+  const [form, setForm] = useState({ username: "", password: "" });
+
+  const handlesChanges = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    axios
+      .post("https://bloomtechrecipebook.herokuapp.com/api/users/login", {
+        username: "dave",
+        password: "dave123",
+      })
+      .then((resp) => console.log(resp))
+      .catch((err) => console.log(err));
+  };
+
+  return (
+    <div className="login-page">
+      <div className="login-box">
+        <div className="login-top">
+          <div className="icon"></div>
+          <br />
+          <span className="welcome-message">Welcome Chef!</span>
         </div>
-    )
-}
+        <div className="login-bottom">
+          <form className="login-form" onSubmit={handleSubmit}>
+            <div className="input-group">
+              <input
+                type="text"
+                name="username"
+                autoComplete="off"
+                className="input-field"
+                placeholder="Username"
+                value={form.username}
+                onChange={handlesChanges}
+              />
+              <label className="input-label">
+                <span>Username</span>
+              </label>
+            </div>
+            <div className="input-group">
+              <input
+                type="password"
+                name="password"
+                className="input-field"
+                placeholder="Password"
+                value={form.password}
+                onChange={handlesChanges}
+              />
+              <label className="input-label">
+                <span>Password</span>
+              </label>
+              {error && <span>*Your username or password is incorrect</span>}
+              <br />
+              <br />
+              <span>
+                Not a member? <a href="/">Sign Up</a>
+              </span>
+            </div>
+            <button>Log In</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default LoginPage;
--- src/components/SignUpPage.js
+++ src/components/SignUpPage.js
@@ -2,81 +2,86 @@ import React, { useState } from "react";
 import "../styles/SignUpPage.css";
 import axios from "axios";
 
-
 const SignUpPage = () => {
-    const [error, setError] = useState(false);
-    const [confirmPW, setConfirmPW] = useState({password: ""});
-    const [form, setForm] = useState({username: "", password: ""});
+  const [error, setError] = useState(false);
+  const [confirmPW, setConfirmPW] = useState({ password: "" });
+  const [form, setForm] = useState({ username: "", password: "" });
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        if(confirmPW.password !== form.password) return
-        axios
-        .post('https://bloomtechrecipebook.herokuapp.com/api/users', form)
-        .then(resp => {setError(false)})
-        .catch(err => console.log(err))
-        .finally(setForm({username: "", password:""}));
-        setError(true);
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (confirmPW.password !== form.password) return;
+    axios
+      .post("https://bloomtechrecipebook.herokuapp.com/api/users", form)
+      .then((resp) => {
+        setError(false);
+      })
+      .catch((err) => console.log(err))
+      .finally(setForm({ username: "", password: "" }));
+    setError(true);
+  };
 
-    const onChange = (e) => {
-        e.preventDefault();
-        setForm({...form, [e.target.name]: e.target.value});
-    }
+  const onChange = (e) => {
+    e.preventDefault();
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
 
-    const confirmPasswordOnChange = (e) => {
-        e.preventDefault();
-        setConfirmPW({...confirmPW, [e.target.name]:e.target.value});
-    }
-    
-    return (
-        <div className="signup-page-container">
-            <div className="signup-box">
-                <div className="signup-top">
-                    
-                </div>
-                <div className="signup-bottom">
-                    <form className="signup-form" onSubmit={handleSubmit}>
-                        <div className="input-group">
-                            <input 
-                            type="text"
-                            name="username"
-                            className="input-field"
-                            placeholder="Username"
-                            value={form.username}
-                            onChange={onChange}
-                            />
-                            <label className="input-label"><span>Username</span></label>
-                        </div>
-                        <div className="input-group">
-                            <input 
-                            type="password"
-                            name="password"
-                            className="input-field"
-                            placeholder="Password"
-                            value={confirmPW.password}
-                            onChange={confirmPasswordOnChange}
-                            />
-                            <label className="input-label"><span>Password</span></label>
-                         </div>
-                         <div className="input-group">
-                            <input 
-                            type="password"
-                            name="password"
-                            className="input-field"
-                            placeholder="Confirm Password"
-                            value={form.password}
-                            onChange={onChange}
-                            />
-                            <label className="input-label"><span>Confirm Password</span></label>
-                            {error && <span>*Your passwords do not match</span>}
-                            <button>Create Account</button>
-                        </div>  
-                    </form>
-                </div>
+  const confirmPasswordOnChange = (e) => {
+    e.preventDefault();
+    setConfirmPW({ ...confirmPW, [e.target.name]: e.target.value });
+  };
+
+  return (
+    <div className="signup-page-container">
+      <div className="signup-box">
+        <div className="signup-top"></div>
+        <div className="signup-bottom">
+          <form className="signup-form" onSubmit={handleSubmit}>
+            <div className="input-group">
+              <input
+                type="text"
+                name="username"
+                className="input-field"
+                placeholder="Username"
+                value={form.username}
+                onChange={onChange}
+              />
+              <label className="input-label">
+                <span>Username</span>
+              </label>
             </div>
+            <div className="input-group">
+              <input
+                type="password"
+                name="password"
+                className="input-field"
+                placeholder="Password"
+                value={confirmPW.password}
+                onChange={confirmPasswordOnChange}
+              />
+              <label className="input-label">
+                <span>Password</span>
+              </label>
+            </div>
+            <div className="input-group">
+              <input
+                type="password"
+                name="password"
+                className="input-field"
+                placeholder="Confirm Password"
+                value={form.password}
+                onChange={onChange}
+              />
+              <label className="input-label">
+                <span>Confirm Password</span>
+              </label>
+              {error && <span>*Your passwords do not match</span>}
+              <button>Create Account</button>
+            </div>
+          </form>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default SignUpPage;
